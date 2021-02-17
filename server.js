7bfdba79cd906c536e98d12f9bf68037ffd84f6f
Solomiia <ssahala@Solomiias-MacBook-Pro.local>
2021-02-17 22:50:18 +0200

    ready for deploy

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-const e = require('express');
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 
 //connect Database
@@ -11,8 +10,8 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) =>
-    res.json({ msg: 'Hello world' }));
+// app.get('/', (req, res) =>
+//     res.json({ msg: 'Hello world' }));
 
 
 //Define Routes
@@ -20,6 +19,17 @@ app.get('/', (req, res) =>
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
+
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    //set static f0lder
+
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('server started'));
